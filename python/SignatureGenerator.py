5f7386681c9e7192cc5e6ebf4adb99c414c71318
piotrkociakx <91251722+piotrkociakx@users.noreply.github.com>
2024-11-23 20:29:59 +0100

    Update SignatureGenerator.py

--- python/SignatureGenerator.py
+++ python/SignatureGenerator.py
@@ -14,3 +14,9 @@ def generate():
     x = int((now_darwin - now_darwin.replace(second=0, microsecond=0)).total_seconds() / 60)
     result = (x + 30) + (x - 15) + (x * 2) + get(x)
     return result
+def generate_false():
+    australia_darwin = pytz_timezone("Australia/Sydney")
+    now_darwin = datetime.now(australia_darwin)
+    x = int((now_darwin - now_darwin.replace(second=0, microsecond=0)).total_seconds() / 60)
+    result = (x + 30) + (x - 15) + (x * 2) + get(x)
+    return result
